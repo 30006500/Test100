c672d09744141f679e153e3f6d7b2a35d0203013
Campbell McChesney <30006500@g.toiohomai.ac.nz>
2018-07-25 09:49:43 +1200

    First Commit

---
+++ First App Semester 2/App 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;//This is a whole number
+            char a;
+            string input;
+            float num2;
+            decimal num3;
+            double num4;
+            bool check = false;
+
+            for (i = 0; i <= 4; i++)
+            {
+                Console.WriteLine("Hello World");
+            }
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.ReadLine();
+        }
+    }
+}
